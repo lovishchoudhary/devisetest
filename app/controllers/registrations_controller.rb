7946d5ea4ac2879317abd1b3c969137286be06df
Rakibul Islam <rakib1987@gmail.com>
2015-08-17 22:51:36 -0400

    User Registration HTML format works!

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
 
- def create
+def create
   @user = User.create(user_params)
   respond_to do |format|
     format.html {
-      @user.save ? (super) : (render :new)
+      @user.save ? (render @user) : (render 'layouts/application')
     }
+
     format.json {
-      @user.save ? (render :json => {:state => {:code => 0}, :data => @user }) : 
+      @user.save ? (render :json => {:state => {:code => 0}, :data => @user }) :
                    (render :json => {:state => {:code => 1, :messages => @user.errors.full_messages} })
     }
   end
