03146488abb7e8dd64aaf47853f272edff04e181
lovishchoudhary <lovishiit@gmail.com>
2015-08-18 15:30:40 +0530

    Corrected html response on signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
+  def create
 
- def create
-  @user = User.create(user_params)
-  respond_to do |format|
-    format.html {
-      @user.save ? (super) : (render :new)
-    }
-    format.json {
-      @user.save ? (render :json => {:state => {:code => 0}, :data => @user }) : 
-                   (render :json => {:state => {:code => 1, :messages => @user.errors.full_messages} })
-    }
+    respond_to do |format|
+      format.html {
+        super
+      }
+      format.json {
+        @user = User.create(user_params)
+        @user.save ? (render :json => {:state => {:code => 0}, :data => @user }) : 
+                     (render :json => {:state => {:code => 1, :messages => @user.errors.full_messages} })
+      }
+    end
   end
-end
 
 private
 
-  def user_params
-     params.require(:user).permit(:email, :password)
-  end
-end 
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+  end 
